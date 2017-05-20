dc923045945a13dc520e3d9af344e73fe9ec98a6
panxzz <william.pankau@gmail.com>
2017-05-19 20:00:22 -0500

    all includes

--- blackout.js
+++ blackout.js
@@ -22,17 +22,17 @@ include("https://rawgit.com/panxzz/NN-blackout/master/lib/jquery-3.2.1.slim.min.
         console.log("jquery loaded");
 
         //check the date/time to see if the blackout is currently going on
-        $.getScript('https://rawgit.com/panxzz/NN-blackout/master/lib/check-date.js', function(){
+        include('https://rawgit.com/panxzz/NN-blackout/master/lib/check-date.js', function(){
 
             //if it is blackout time then "break" the page
-            $.getScript('https://rawgit.com/panxzz/NN-blackout/master/lib/break-stuff.js', function(){
+            include('https://rawgit.com/panxzz/NN-blackout/master/lib/break-stuff.js', function(){
 
                 //after 10 seconds or any click on the page pop the modal
                 window.setTimeout(popModal, 10000);
             });
 
             var popModal = function(){
-                $.getScript('https://rawgit.com/panxzz/NN-blackout/master/lib/modal-display.js', function(){
+                include('https://rawgit.com/panxzz/NN-blackout/master/lib/modal-display.js', function(){
                     //modal is displayed
                 });
             }
