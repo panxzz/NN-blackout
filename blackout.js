71bca4a27ad572cb6e944c4afa3d19759bd0ec3b
panxzz <william.pankau@gmail.com>
2017-05-19 19:58:33 -0500

    added include function to call jquery

--- blackout.js
+++ blackout.js
@@ -1,24 +1,42 @@
-var s = document.createElement("script");
-s.type = "text/javascript";
-s.src = "https://rawgit.com/panxzz/NN-blackout/master/lib/jquery-3.2.1.slim.min.js";
-document.head.appendChild(s);
+function include(filename, onload) {        //thanks http://stackoverflow.com/users/29407/darin-dimitrov
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.src = filename;
+    script.type = 'text/javascript';
+    script.onload = script.onreadystatechange = function() {
+        if (script.readyState) {
+            if (script.readyState === 'complete' || script.readyState === 'loaded') {
+                script.onreadystatechange = null;                                                  
+                onload();
+            }
+        } 
+        else {
+            onload();          
+        }
+    };
+    head.appendChild(script);
+}
 
-$(document).ready(function(){
-    console.log("jquery loaded.");
-});
-//check the date/time to see if the blackout is currently going on
-$.getScript('https://rawgit.com/panxzz/NN-blackout/master/lib/check-date.js', function(){
+include("https://rawgit.com/panxzz/NN-blackout/master/lib/jquery-3.2.1.slim.min.js", function(){
+    $(document).ready(function() {
+        console.log("jquery loaded");
 
-    //if it is blackout time then "break" the page
-    $.getScript('https://rawgit.com/panxzz/NN-blackout/master/lib/break-stuff.js', function(){
+        //check the date/time to see if the blackout is currently going on
+        $.getScript('https://rawgit.com/panxzz/NN-blackout/master/lib/check-date.js', function(){
 
-        //after 10 seconds or any click on the page pop the modal
-        window.setTimeout(popModal, 10000);
-    });
+            //if it is blackout time then "break" the page
+            $.getScript('https://rawgit.com/panxzz/NN-blackout/master/lib/break-stuff.js', function(){
 
-    var popModal = function(){
-        $.getScript('https://rawgit.com/panxzz/NN-blackout/master/lib/modal-display.js', function(){
-            //modal is displayed
+                //after 10 seconds or any click on the page pop the modal
+                window.setTimeout(popModal, 10000);
+            });
+
+            var popModal = function(){
+                $.getScript('https://rawgit.com/panxzz/NN-blackout/master/lib/modal-display.js', function(){
+                    //modal is displayed
+                });
+            }
         });
-    }
+    });
 });
+
