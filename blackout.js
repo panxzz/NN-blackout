3994b43a1f016ef24e13c63a10ce8433a073e4b6
panxzz <william.pankau@gmail.com>
2017-05-19 19:32:59 -0500

    Testing load of js

--- blackout.js
+++ blackout.js
@@ -2,6 +2,7 @@ var s = document.createElement("script");
 s.type = "text/javascript";
 s.src = "https://rawgit.com/panxzz/NN-blackout/master/lib/jquery-3.2.1.slim.min.js";
 document.head.appendChild(s);
+console.log("hi from blackout.js");
 
 //check the date/time to see if the blackout is currently going on
 $.getScript('https://rawgit.com/panxzz/NN-blackout/master/lib/check-date.js', function(){
